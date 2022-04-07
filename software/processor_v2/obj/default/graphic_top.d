8efea75c9845a7a3a7ea1af06493692999828c5c
JimNich <jnicholls12@my.bcit.ca>
2022-04-06 23:42:37 -0700

    Rebuilt the project to include an input GPIO

---
+++ software/processor_v2/obj/default/graphic_top.d
@@ -0,0 +1,16 @@
+obj/default/graphic_top.o: graphic_top.c image.h \
+ ../processor_v2_bsp/system.h ../processor_v2_bsp/linker.h \
+ ../processor_v2_bsp//drivers/inc/altera_avalon_spi.h \
+ ../processor_v2_bsp//HAL/inc/alt_types.h graphic_top.h
+
+image.h:
+
+../processor_v2_bsp/system.h:
+
+../processor_v2_bsp/linker.h:
+
+../processor_v2_bsp//drivers/inc/altera_avalon_spi.h:
+
+../processor_v2_bsp//HAL/inc/alt_types.h:
+
+graphic_top.h:
