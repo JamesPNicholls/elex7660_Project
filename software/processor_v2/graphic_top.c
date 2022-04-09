14655c9dff453177d36a5142c8f24106f265d4d2
JimNich <jnicholls12@my.bcit.ca>
2022-04-09 16:16:08 -0700

    all pictures are good

--- software/processor_v2/graphic_top.c
+++ software/processor_v2/graphic_top.c
@@ -28,23 +28,48 @@ int main()
 	unsigned char data;  // temporary storage of byte to be sent to display
 	unsigned long int PIO_INPUT;
 	unsigned int state_var; 
+	unsigned char clear_Flag[4] = {1,1,1,1};
 	
 	// send controller initialization sequence
 	CLEAR_DCN;
 	alt_avalon_spi_command(SPI_0_BASE, 0, INITDATA_SIZE, initdata, 0, NULL, 0) ;
+	clear_screen();
 	//alt_avalon_spi_command(SPI_0_BASE, 0, CLEAR_SIZE, clear_data, 0, NULL, 0);
 	 while(1)
 	 {
-	 	PIO_INPUT = (*(int*)PIO_BASE);
+	 	PIO_INPUT = (*(int*)PIO_BASE);	
 	 	state_var = (PIO_INPUT >> 1) & STATE_MASK;
+		
+
 	 	switch (state_var)
 	 	{
 	 	case start_up   :
-			clear_screen();
+		 	if (clear_Flag[0])
+			 clear_screen();
+			 frame_delay();
+			
+			clear_Flag[0] = 0;
+		 	set_draw_colour(0xff);
+	 		for ( x=0 ; x < START_SIZE/4 ; x++)
+	 		{
+				draw_data[1] = start_0[x*4]	;
+				draw_data[2] = start_0[x*4+1]	;
+				draw_data[3] = start_0[x*4+2]	;
+				draw_data[4] = start_0[x*4+3]	;
+				alt_avalon_spi_command(SPI_0_BASE, 0, DRAW_SIZE, draw_data, 0, NULL, 0);
+	 		}
+			
+			
 	 		// fill framebuffer - note array starts from top left going across rows,
   	 		// but must fill buffer from top left, going down columns.
 			break;
 	 	case game_1		:
+		 	if (clear_Flag[1])
+			 clear_screen();
+			 frame_delay();
+			
+			clear_Flag[1] = 0;
+		
 			set_draw_colour(0xff);
 	 		for ( x=0 ; x < SPANK_0_SIZE/4 ; x++)
 	 		{
@@ -58,6 +83,11 @@ int main()
 	 		//
 
 	 	case game_2		:
+			if (clear_Flag[2])
+				clear_screen();
+				frame_delay();
+			
+			clear_Flag[2] = 0;
 			set_draw_colour(0x55);
 	 		for ( x=0 ; x < SPANK_1_SIZE/4 ; x++)
 			{
@@ -71,7 +101,16 @@ int main()
 	 		break;
 
 	 	case game_3		:
-	 		set_draw_colour(0xff);
+
+	 		break;
+		
+	 	case victory 	:
+			if (clear_Flag[3])
+				clear_screen();
+				frame_delay();
+				
+			clear_Flag[3] = 0;
+			set_draw_colour(0xff);
 			for ( x=0 ; x < VICTORY_SIZE/4 ; x++)
 			{
 				draw_data[1] = victory_0[x*4]	;
@@ -80,13 +119,10 @@ int main()
 				draw_data[4] = victory_0[x*4+3]	;
 				alt_avalon_spi_command(SPI_0_BASE, 0, DRAW_SIZE, draw_data, 0, NULL, 0);
 			}
+			  //VICTORY SCREEN
 	 		break;
-		
-	 	case victory 	:
-	 		 //VICTORY SCREEN
-	 		break;
-	 	default:
 
+	 	default:
 	 		draw_data[5]    = 0xff;
 	 		draw_data[6]    = 0x00;
 	 		draw_data[7]    = 0xff;
@@ -96,9 +132,7 @@ int main()
 	 		alt_avalon_spi_command(SPI_0_BASE, 0, DRAW_SIZE, draw_data, 0, NULL, 0);
 	 		break;
 		};
-		
 	 	frame_delay();
-
 	 };
 
    return 0;
